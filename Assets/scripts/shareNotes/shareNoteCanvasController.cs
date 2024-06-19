fb95be793760b3f875732c5d5a4dd4dc557f0aa0
dhruvds12 <dhruv.devgan@yahoo.com>
2024-06-19 14:02:07 +0100

    Removed firebase and added shared notes controller

--- Assets/scripts/shareNotes/shareNoteCanvasController.cs
+++ Assets/scripts/shareNotes/shareNoteCanvasController.cs
@@ -7,20 +7,24 @@ public class shareNoteCanvasController : MonoBehaviour
 {
     public Canvas shareNoteCanvas; // Reference to the settings menu canvas
 
+
     void Start()
     {
         shareNoteCanvas.enabled = false; // Hide the settings menu canvas on start
+
     }
 
     // Method to show the settings menu
     public void ShowshareNoteCanvas()
     {
         shareNoteCanvas.enabled = true;
+
     }
 
     // Method to hide the settings menu
     public void HideshareNoteCanvas()
     {
         shareNoteCanvas.enabled = false;
+
     }
 }
---
+++ Assets/scripts/shareNotes/shareNotesController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class ShareNoteController : MonoBehaviour
+{
+    public GameObject userListPopup; // Reference to the UserListPopup
+    public Button shareButton; // Reference to the ShareButton
+    public Transform userListContent; // Reference to the Scroll View Content
+    public GameObject userButtonPrefab; // Reference to the Button prefab for users
+
+    private ApiManager apiManager;
+
+    void Start()
+    {
+        shareButton.onClick.AddListener(OpenUserListPopup);
+        apiManager = FindObjectOfType<ApiManager>();
+        userListPopup.SetActive(false);
+    }
+
+    void OpenUserListPopup()
+    {
+        userListPopup.SetActive(true);
+        StartCoroutine(apiManager.GetUsers(OnUsersLoaded));
+    }
+
+    void OnUsersLoaded(List<ApiManager.User> users)
+    {
+        // Clear existing user buttons
+        foreach (Transform child in userListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Populate the user list with buttons
+        foreach (var user in users)
+        {
+            if (user.userId == "8e005a538ebadc51ac94cf080ca3d5ba") // Skip the current user
+                continue;
+
+            GameObject userButton = Instantiate(userButtonPrefab, userListContent);
+            TextMeshProUGUI buttonText = userButton.GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = user.username;
+            userButton.GetComponent<Button>().onClick.AddListener(() => ShareNoteWithUser(user.userId));
+        }
+    }
+
+    void ShareNoteWithUser(string userId)
+    {
+        StartCoroutine(apiManager.ShareNote("8e005a538ebadc51ac94cf080ca3d5ba", "image1", "general_note", userId));
+        //userListPopup.SetActive(false); // Close the popup after sharing
+    }
+}
